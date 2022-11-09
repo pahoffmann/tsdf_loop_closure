073993cc7590e26a53e35dc8823a34b36d13f588
pahoffmann <pahoffmann@uni-osnabrueck.de>
2022-11-09 01:11:58 +0100

    thesis update, full fitness score window eval

--- scripts/benchmark_enrichment_window.py
+++ scripts/benchmark_enrichment_window.py
@@ -9,13 +9,13 @@ import general_purpose as gp
 if __name__ == '__main__':
     labels = []
 
-    df = pd.read_csv("./data/fitness_scores.csv")
+    df = pd.read_csv("./data/fitness_scores_full.csv")
     df = df.dropna(axis=1, how='all')
     df = gp.transpose_by_iteration(df, ["x = 0", "x = 1", "x = 2","x = 3","x = 4","x = 5","x = 6",])
     #plot_average_error(df, label="lm", n_measurements=5, random=False)
             
     # plot individual plots
-    gp.plot_error(df, False,1)
+    gp.plot_error(df, True,10)
     #plot_average_iterations(df)
 
     plt.xlabel("Pose-Index")
