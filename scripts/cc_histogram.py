abbda4dccdea47469923b0cd204a8c995180c32e
Patrick Hoffmann <paddy.123456@web.de>
2022-11-16 16:11:48 +0100

    added stuff

--- scripts/cc_histogram.py
+++ scripts/cc_histogram.py
@@ -11,7 +11,8 @@ import math
 if __name__ == '__main__':
     labels = []
 
-    df = pd.read_csv("./data/Histogram_CC.csv", delimiter=';')
+    # df = pd.read_csv("./data/Histogram_point_to_point.csv", delimiter=';')
+    df = pd.read_csv("./data/Histogram_point_to_mesh.csv", delimiter=';')
     df = df.dropna(axis=1, how='all')
     #df = df.transpose()
 
@@ -23,7 +24,7 @@ if __name__ == '__main__':
         x = np.concatenate([x, np.repeat((df[" Class start"][i] + (df[" Class end"][i] - df[" Class start"][i]) / 2), df[" Value"][i])])
 
     print(x)
-    ax = sns.histplot(x, kde=False, stat="density")
+    #ax = sns.histplot(x, kde=False, stat="density")
     plt.xlabel("absolute Distanz [mm]")
     plt.ylabel("Dichte [%]")
 
@@ -31,14 +32,23 @@ if __name__ == '__main__':
 
     print("Mean: " + str(mean))
     
-    mu = 35.89
-    sigma = 54.698
-    values = np.linspace(mu - 3*sigma, mu + 3*sigma, 100)
-    #plt.plot(values, stats.norm.pdf(values, mu, sigma), color="red", label="Gauss-Verteilung")
+    # mu = 35.89
+    # sigma = 54.698
+    # values = np.linspace(mu - 3*sigma, mu + 3*sigma, 100)
+    # plt.plot(values, stats.norm.pdf(values, mu, sigma), color="red", label="Gauss-Verteilung")
+    mu, sigma = stats.norm.fit(x)
+    print("sigma: " + str(sigma))
+    print("mu: " + str(mu))
+
+    plt.hist(x, bins=500, density=True, alpha=0.6, color='#1071e5')
+
+    xmin, xmax = plt.xlim()
+    values = np.linspace(xmin, xmax, 560)
+    plt.plot(values, stats.norm.pdf(values, mu, sigma), color="#e81313", label="Gauss-Verteilung")
     # plt.axvline(x = 15, color = 'green', label = '< 50%')
     # plt.axvline(x = 67, color = 'orange', label = '< 80%')
     # plt.axvline(x = 126, color = 'purple', label = '< 95%')
-    plt.axvline(x = mu, color = 'red', label = 'Mittelwert')
+    plt.axvline(x = mu, color = '#ba23f6', label = 'Mittelwert')
 
     # calculate the pdf
     # x0, x1 = ax.get_xlim()  # extract the endpoints for the x-axis
